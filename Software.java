854c033a9b3b1d7069167613d2b5d7039edfa28f
mostafa-elrosasy <31372214+mostafa-elrosasy@users.noreply.github.com>
2018-04-09 16:12:46 +0200

    Add files via upload

---
+++ Software.java
@@ -0,0 +1,19 @@
+
+package software;
+
+/**
+ *
+ * @author mostafa
+ */
+public class Software {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        mainPage P = new mainPage();
+        P.setVisible(true);
+    }
+    
+}
+
---
+++ mainPage.java
@@ -0,0 +1,84 @@
+
+package software;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Locale;
+import javax.swing.*;
+/**
+ *
+ * @author mostafa
+ */
+public class mainPage extends JFrame {
+    private JLabel codelbl =new JLabel("Enter The Program Code :");
+    private JLabel usernamelbl =new JLabel("User Name :");
+    private JLabel passwordlbl =new JLabel("Password :");
+    
+    private JTextField codetxt =new JTextField(64);
+    private JTextField usernametxt =new JTextField(64);
+    private JTextField passwordtxt =new JTextField(64);
+        
+    private JButton entercode = new JButton("Enter Program Code");
+    private JButton login = new JButton("Login");
+        
+    mainPage(){
+        init();
+    }
+
+    private void init() {
+        this.setSize(400,400);
+        this.setDefaultCloseOperation(3);
+        this.setLayout(null);
+        this.setResizable(false);
+        this.setTitle("HMS");
+//////////////////////////////////////////////////////Labels        
+        codelbl.setBounds(10,10 ,160, 20);
+        this.add(codelbl);
+ 
+        usernamelbl.setBounds(10,150 ,150, 20);
+        this.add(usernamelbl);
+        
+        passwordlbl.setBounds(10,200 ,150, 20);
+        this.add(passwordlbl);
+///////////////////////////////////////////////////// TextFields
+        codetxt.setBounds(110,60,150,20);
+        this.add(codetxt);
+        
+        usernametxt.setBounds(170,150,150,20);
+        this.add(usernametxt);
+        
+        passwordtxt.setBounds(170,200,150,20);
+        this.add(passwordtxt);
+//////////////////////////////////////////////////// Buttons
+        entercode.setBounds(85,110,200,20);
+        entercode.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                int n =Integer.parseInt(codetxt.getText());
+                if(n==0){
+                    AdminPage P =new AdminPage();
+                    close();
+                    P.setVisible(true);
+                }
+                
+            }
+        });
+        this.add(entercode);
+        
+        login.setBounds(170,250,150,20);
+        login.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                int n = Integer.parseInt(passwordtxt.getText());
+                String name=usernametxt.getText();
+                if(name.equals("admin")&&n==0){
+                    HMS P =new HMS();
+                    close();
+                    P.setVisible(true);
+                }
+            }
+        });
+        this.add(login);
+    }
+    public void close(){
+        this.dispose();
+    }
+    
+}
